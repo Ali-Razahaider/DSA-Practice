f59f6d4a22c920fd1ac184bc5dd2ccd5e0e81a7c
Ali <alirazahaider2@gmail.com>
2025-02-27 00:06:18 +0500

    Refactor next permutation implementation to use std::swap and std::reverse for clarity

--- nextPerm.cpp
+++ nextPerm.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <utility>  // For std::swap
 
 class Solution {
 public:
@@ -15,17 +16,17 @@ public:
             }
         }
         if (ind == -1) {
-            reverse(nums.begin(), nums.end());
+            std::reverse(nums.begin(), nums.end());
             return;
         }
 
         for (int i = n - 1; i > ind; i--) {
             if (nums[i] > nums[ind]) {
-                swap(nums[i], nums[ind]);
+                std::swap(nums[i], nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+1+ind, nums.end());
+        std::reverse(nums.begin()+1+ind, nums.end());
     }
 };
 
