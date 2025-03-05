01cbfe7a252091291a2f63e9712c3c23f6dd1c41
Ali <alirazahaider2@gmail.com>
2025-03-06 00:08:13 +0500

    Add second method for finding majority elements using Boyer-Moore Voting Algorithm

--- majorityEl2.cpp
+++ majorityEl2.cpp
@@ -25,6 +25,47 @@ public:
         }
         return rr;
     }
+
+    // The second approach uses Boyer-Moore Voting Algorithm
+    // Time Complexity: O(n), Space Complexity: O(1)
+    vector<int> majorityElement2(vector<int>& nums) {
+        vector<int> maj;
+        int cnt = 0;
+        int cnt1 = 0;
+        int cand = -1;
+        int cand2 = -1;
+        vector<int> majority;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == cand)
+                cnt++;
+            else if (nums[i] == cand2)
+                cnt1++;
+            else if (cnt1 == 0) {
+                cand2 = nums[i];
+                cnt1 = 1;
+            } else if (cnt == 0) {
+                cand = nums[i];
+                cnt = 1;
+            } else {
+                cnt--;
+                cnt1--;
+            }
+        }
+        cnt = cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == cand)
+                cnt++;
+            else if (nums[i] == cand2)
+                cnt1++;
+        }
+        if (cnt > n / 3)
+            maj.push_back(cand);
+
+        if (cnt1 > n / 3)
+            maj.push_back(cand2);
+        return maj;
+    }
 };
 
 // Driver code
@@ -39,7 +80,7 @@ int main() {
         {2, 2, 1, 1, 1, 2, 2} // Expected: [1, 2]
     };
     
-    // Process each test case
+    // Process each test case with both methods
     for (int i = 0; i < testCases.size(); i++) {
         cout << "Test Case " << i + 1 << ": ";
         
@@ -49,21 +90,45 @@ int main() {
             cout << testCases[i][j];
             if (j < testCases[i].size() - 1) cout << ", ";
         }
-        cout << "], ";
+        cout << "]" << endl;
         
-        // Get and print result
-        vector<int> result = sol.majorityElement(testCases[i]);
-        cout << "Output: [";
-        for (int j = 0; j < result.size(); j++) {
-            cout << result[j];
-            if (j < result.size() - 1) cout << ", ";
+        // Get and print result for method 1
+        vector<int> result1 = sol.majorityElement(testCases[i]);
+        cout << "  Method 1 Output: [";
+        for (int j = 0; j < result1.size(); j++) {
+            cout << result1[j];
+            if (j < result1.size() - 1) cout << ", ";
         }
         cout << "]" << endl;
+        
+        // Get and print result for method 2
+        vector<int> result2 = sol.majorityElement2(testCases[i]);
+        cout << "  Method 2 Output: [";
+        for (int j = 0; j < result2.size(); j++) {
+            cout << result2[j];
+            if (j < result2.size() - 1) cout << ", ";
+        }
+        cout << "]" << endl;
+        
+        // Compare results
+        bool resultsMatch = (result1.size() == result2.size());
+        if (resultsMatch) {
+            unordered_map<int, int> elementCount;
+            for (int num : result1) elementCount[num]++;
+            for (int num : result2) elementCount[num]--;
+            for (auto& pair : elementCount) {
+                if (pair.second != 0) {
+                    resultsMatch = false;
+                    break;
+                }
+            }
+        }
+        cout << "  Results match: " << (resultsMatch ? "Yes" : "No") << endl << endl;
     }
     
     // Custom input
     int n;
-    cout << "\nEnter the size of your custom array: ";
+    cout << "Enter the size of your custom array: ";
     cin >> n;
     
     vector<int> customArray(n);
@@ -72,13 +137,26 @@ int main() {
         cin >> customArray[i];
     }
     
-    vector<int> customResult = sol.majorityElement(customArray);
-    cout << "Elements appearing more than n/3 times: [";
-    for (int j = 0; j < customResult.size(); j++) {
-        cout << customResult[j];
-        if (j < customResult.size() - 1) cout << ", ";
+    // Test both methods with custom input
+    vector<int> customResult1 = sol.majorityElement(customArray);
+    cout << "Method 1 - Elements appearing more than n/3 times: [";
+    for (int j = 0; j < customResult1.size(); j++) {
+        cout << customResult1[j];
+        if (j < customResult1.size() - 1) cout << ", ";
     }
     cout << "]" << endl;
+    
+    vector<int> customResult2 = sol.majorityElement2(customArray);
+    cout << "Method 2 - Elements appearing more than n/3 times: [";
+    for (int j = 0; j < customResult2.size(); j++) {
+        cout << customResult2[j];
+        if (j < customResult2.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+    
+    // Compare execution time (optional)
+    cout << "\nNote: Method 1 uses a hashmap approach (O(n) time, O(n) space)" << endl;
+    cout << "      Method 2 uses Boyer-Moore Voting Algorithm (O(n) time, O(1) space)" << endl;
     
     return 0;
 }
