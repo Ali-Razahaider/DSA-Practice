ec56f8f1a8651d34868953f1689d6c6865a47489
Ali <alirazahaider2@gmail.com>
2025-03-12 19:59:49 +0500

    Add implementation for finding two elements in an array that are missing and duplicated naive approach

---
+++ repeatnMiss.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        vector<int>ans;
+        
+        unordered_map<int,int>map;
+        
+        for(int i=0 ; i < arr.size();i++){
+            map[arr[i]]++;
+        }
+        int n =-1;
+        int n2=-1;
+        for(int i = 1 ;i<=arr.size();i++){
+            if(map[i] == 0)n = i;
+            else if(map[i]==2)n2 = i;
+        }
+        ans.push_back(n2);
+        ans.push_back(n);
+        return ans;
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(arr);
+        cout << ans[0] << " " << ans[1] << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
